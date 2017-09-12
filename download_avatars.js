ab899fb0075cf65bebff1d85afec78ae980c9e8c
Scotty <scott.hagan@aol.com>
2017-09-12 00:15:22 +0000

    added request url to getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
 const request = require('request');
 
-
+const gitUser = process.env.GITHUB_USER;
+const gitToken= process.env.GITHUB_TOKEN;
 function getRepoContributors(repoOwner, repoName, cb) {
-
+  var requestURL = 'https://'+ gitUser + ':' + gitToken + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
